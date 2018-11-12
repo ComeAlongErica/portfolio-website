cd31a9efad28a929228312e8e4bc0f6582e5b794
chris <christopher.plemmons@gmail.com>
2018-11-12 14:59:01 -0500

    starter kit

---
+++ module/module.js
@@ -0,0 +1,13 @@
+"use strict";
+
+angular
+    .module("App", ["ngRoute"])
+    // .config(["$routeProvider", ($routeProvider) => {   WE NEED TO UPDATE THIS, IT'S JUST FOR REFERENCE!
+    //     $routeProvider
+    //     .when("/user-profile", {
+    //         template: "<user-profile></user-profile>"
+    //     })
+    //     .when("/edit-profile", {
+    //         template: "<edit-profile></edit-profile>"
+    //     }).otherwise({redirectTo:"/user-profile"})
+    // }])
