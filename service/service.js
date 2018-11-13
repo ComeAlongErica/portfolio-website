7cd81ab43347b547d9079fd08e48783c6e253263
ComeAlongErica <ericaraephoto@gmail.com>
2018-11-13 10:05:29 -0500

    working on sending date to api request

--- service/service.js
+++ service/service.js
@@ -1,32 +1,45 @@
 "use strict";
 
-function Service($location, $http) {
+function EventService($location, $http) {
     const self = this;
     let jsonPayload = null;
-    
-    self.searchTicketMaster = (searchKeyword, postalCode, localDate) => {
-        console.log(searchKeyword);
-        console.log(postalCode);
-        console.log(localDate);
+    let searchResults;
+
+    self.submitData = (search) => {
+        searchResults = search;
+        self.searchTicketMaster(search);
+        // console.log(searchResults);
+    };
+
+    self.searchTicketMaster = (data) => {
+        console.log(search.localDate.getFullYear());
+        console.log(search.localDate.getDate());
+        console.log(search.localDate.getMonth() + 1);
+
+        // console.log("You're in api")
+        // console.log(data.searchKeyword);
+        // console.log(data.postalCode);
+        // console.log(data.localDate);
+
         return $http({
             method: "GET", // Defines the method
-            
+
             url: `http://app.ticketmaster.com/discovery/v2/events.json?keyword=${searchKeyword}&postalCode=${postalCode}&localDate=${localDate}&apikey=ibBJCTVGbVNR0NGGSUX7I2MLXS17aVQH` // Defines the URL
-          }).then((data) => {
+        }).then((data) => {
             // This method is what is used to get data from the promise once it has been resolved
             jsonPayload = data;
             console.log(jsonPayload)
             // Returns the jsonPayload variable
             return jsonPayload;
-          });
+        });
     }
 
 
 };
 angular
     .module("App")
-    .service("Service",Service);
+    .service("EventService", EventService);
 
-    
+
 
     //https://app.ticketmaster.com/discovery/v2/events.json?keyword=${searchKeyword}&postalCode=${postalCode}&startDateTime=${startDateTime}&apikey=ibBJCTVGbVNR0NGGSUX7I2MLXS17aVQH//
