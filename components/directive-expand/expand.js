1169d4f1ba0a9f430955aca3e4f34f14160d4485
ComeAlongErica <ericaraephoto@gmail.com>
2018-11-14 14:04:58 -0500

    fixed our really awesome directive again haha

--- components/directive-expand/expand.js
+++ components/directive-expand/expand.js
@@ -3,10 +3,13 @@
 function expandOnClick() {
   return {
     restrict: "A",
-    link: function($scope, $element, $attrs) {
+    link: function ($scope, $element, $attrs) {
       $element.on("click", () => {
         console.log($element);
-        $element[0].nextElementSibling.classList.toggle("hidden");
+        if ($element[0].tagName === "P") {
+          console.log($element[0]);
+          $element[0].parentElement.nextElementSibling.classList.toggle("hidden");
+        }
       });
     }
   };
