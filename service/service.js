3ac4e97d49aedf0a31e499c2b98cadb0b31d52d4
ComeAlongErica <ericaraephoto@gmail.com>
2018-11-21 10:17:30 -0500

    remove route in service

--- service/service.js
+++ service/service.js
@@ -3,20 +3,5 @@
 function PortService($location) {
     const self = this;
 
-
-    self.updateHead = (route) => {
-
-        if (route === "home") {
-            $location.path("/home");
-        } else if (route === "about") {
-            $location.path("/about");
-        } else if (route === "contact") {
-            $location.path("/contact");
-        } else {
-            $location.path("/home");
-        };
-
-    }
-
 };
 angular.module("Portfolio").service("PortService", PortService);
