5c08bc22945150ef51b5aa3e8a1fb5c130f978e5
ComeAlongErica <ericaraephoto@gmail.com>
2018-11-22 10:25:15 -0500

    optimize iamages

--- components/projects/projects.js
+++ components/projects/projects.js
@@ -6,11 +6,11 @@ const projects = {
     controller: ["PortService", function (PortService) {
         const vm = this;
         vm.projectDisplay = [
-            {name: "Project 1", img: "images/event.png", url: "https://github.com/ComeAlongErica/event-scout-event-website"},
-            {name: "Project 2", img: "images/todo.png", url: "https://github.com/ComeAlongErica/todo-list-angular-js"},
-            {name: "Project 3", img: "images/bioshock.png", url: "https://github.com/ComeAlongErica/bioshock-plasmid-shopping-cart"},
-            {name: "Project 3", img: "images/dogthoughts.png", url: "https://github.com/ComeAlongErica/mock-twitter-feed-angular"},
-            {name: "Project 3", img: "images/book-contacts.png", url: "https://github.com/ComeAlongErica/js-address-book"},  
+            {name: "Project 1", img: "images/event.jpg", url: "https://github.com/ComeAlongErica/event-scout-event-website"},
+            {name: "Project 2", img: "images/todo.jpg", url: "https://github.com/ComeAlongErica/todo-list-angular-js"},
+            {name: "Project 3", img: "images/bioshock.jpg", url: "https://github.com/ComeAlongErica/bioshock-plasmid-shopping-cart"},
+            {name: "Project 3", img: "images/dogthoughts.jpg", url: "https://github.com/ComeAlongErica/mock-twitter-feed-angular"},
+            {name: "Project 3", img: "images/book-contacts.jpg", url: "https://github.com/ComeAlongErica/js-address-book"},  
         ]
 
     }]
