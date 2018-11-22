08368b31fead27850b565013444503a115010196
ComeAlongErica <ericaraephoto@gmail.com>
2018-11-22 10:27:31 -0500

    fix url

--- components/projects/projects.js
+++ components/projects/projects.js
@@ -9,7 +9,7 @@ const projects = {
             {name: "Project 1", img: "images/event.jpg", url: "https://github.com/ComeAlongErica/event-scout-event-website"},
             {name: "Project 2", img: "images/todo.jpg", url: "https://github.com/ComeAlongErica/todo-list-angular-js"},
             {name: "Project 3", img: "images/bioshock.jpg", url: "https://github.com/ComeAlongErica/bioshock-plasmid-shopping-cart"},
-            {name: "Project 3", img: "images/dogthoughts.jpg", url: "https://github.com/ComeAlongErica/mock-twitter-feed-angular"},
+            {name: "Project 3", img: "images/dogthought.jpg", url: "https://github.com/ComeAlongErica/mock-twitter-feed-angular"},
             {name: "Project 3", img: "images/book-contacts.jpg", url: "https://github.com/ComeAlongErica/js-address-book"},  
         ]
 
