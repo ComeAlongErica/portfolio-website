08ead4fb48459412764e5e78347186d67e6594a4
ComeAlongErica <ericaraephoto@gmail.com>
2018-11-26 16:58:18 -0500

    finish hover directive

--- components/projects/projects.js
+++ components/projects/projects.js
@@ -6,11 +6,11 @@ const projects = {
     controller: ["PortService", function (PortService) {
         const vm = this;
         vm.projectDisplay = [
-            {name: "Project 1", img: "images/event.jpg", url: "https://github.com/ComeAlongErica/event-scout-event-website"},
-            {name: "Project 2", img: "images/todo.jpg", url: "https://github.com/ComeAlongErica/todo-list-angular-js"},
-            {name: "Project 3", img: "images/bioshock.jpg", url: "https://github.com/ComeAlongErica/bioshock-plasmid-shopping-cart"},
-            {name: "Project 3", img: "images/dogthought.jpg", url: "https://github.com/ComeAlongErica/mock-twitter-feed-angular"},
-            {name: "Project 3", img: "images/book-contacts.jpg", url: "https://github.com/ComeAlongErica/js-address-book"},  
+            {name: "Event Scout", img: "images/event.jpg", url: "https://github.com/ComeAlongErica/event-scout-event-website", description: "An event application using TicketMaster API and Angular."},
+            {name: "To-Do List", img: "images/todo.jpg", url: "https://github.com/ComeAlongErica/todo-list-angular-js", description: "An Angular application that displays, edits, and deletes to-dos."},
+            {name: "Plasmids Shopping Cart", img: "images/bioshock.jpg", url: "https://github.com/ComeAlongErica/bioshock-plasmid-shopping-cart", description: "Shopping cart project using HTML, CSS, JavaScript, and jQuery."},
+            {name: "Dog Thoughts Feed", img: "images/dogthought.jpg", url: "https://github.com/ComeAlongErica/mock-twitter-feed-angular", description: "Use components to construct a list of small posts, similar to Twitter."},
+            {name: "Address Book", img: "images/book-contacts.jpg", url: "https://github.com/ComeAlongErica/js-address-book", description: "Build an object-oriented console address book application."},  
         ]
 
     }]
