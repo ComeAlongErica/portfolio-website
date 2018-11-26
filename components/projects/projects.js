8c0a7ab2dc1a7e5cc6d22a6dbd81f0422f0f48f3
ComeAlongErica <ericaraephoto@gmail.com>
2018-11-26 18:42:52 -0500

    put data in service

--- components/projects/projects.js
+++ components/projects/projects.js
@@ -5,13 +5,8 @@ const projects = {
     bindings: {},
     controller: ["PortService", function (PortService) {
         const vm = this;
-        vm.projectDisplay = [
-            {name: "Event Scout", img: "images/event.jpg", url: "https://github.com/ComeAlongErica/event-scout-event-website", description: "An event application using TicketMaster API and Angular."},
-            {name: "To-Do List", img: "images/todo.jpg", url: "https://github.com/ComeAlongErica/todo-list-angular-js", description: "An Angular application that displays, edits, and deletes to-dos."},
-            {name: "Plasmids Shopping Cart", img: "images/bioshock.jpg", url: "https://github.com/ComeAlongErica/bioshock-plasmid-shopping-cart", description: "Shopping cart project using HTML, CSS, JavaScript, and jQuery."},
-            {name: "Dog Thoughts Feed", img: "images/dogthought.jpg", url: "https://github.com/ComeAlongErica/mock-twitter-feed-angular", description: "Use components to construct a list of small posts, similar to Twitter."},
-            {name: "Address Book", img: "images/book-contacts.jpg", url: "https://github.com/ComeAlongErica/js-address-book", description: "Build an object-oriented console address book application."},  
-        ]
+
+        vm.projectDisplay = PortService.getProjects();
 
     }]
 
--- service/service.js
+++ service/service.js
@@ -3,5 +3,18 @@
 function PortService($location) {
     const self = this;
 
+    self.projects = [
+        {name: "Event Scout", img: "images/event.jpg", url: "https://github.com/ComeAlongErica/event-scout-event-website", description: "An event application using TicketMaster API and Angular."},
+        {name: "To-Do List", img: "images/todo.jpg", url: "https://github.com/ComeAlongErica/todo-list-angular-js", description: "An Angular application that displays, edits, and deletes to-dos."},
+        {name: "Plasmids Shopping Cart", img: "images/bioshock.jpg", url: "https://github.com/ComeAlongErica/bioshock-plasmid-shopping-cart", description: "Shopping cart project using HTML, CSS, JavaScript, and jQuery."},
+        {name: "Dog Thoughts Feed", img: "images/dogthought.jpg", url: "https://github.com/ComeAlongErica/mock-twitter-feed-angular", description: "Use components to construct a list of small posts, similar to Twitter."},
+        {name: "Address Book", img: "images/book-contacts.jpg", url: "https://github.com/ComeAlongErica/js-address-book", description: "Build an object-oriented console address book application."},  
+        {name: "Shopping List", img: "images/grocery.jpg", url: "https://github.com/ComeAlongErica/shopping-cart-database", description: "Create a database for shopping cart app, with front-end functionality."},  
+    ]
+
+    self.getProjects = () => {
+        return self.projects;
+    }
+
 };
 angular.module("Portfolio").service("PortService", PortService);
